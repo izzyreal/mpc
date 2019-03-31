662f31e4f8d50b98f38685e4a2b7b614c6739e76
Izmar <izmaelverhage@gmail.com>
2019-03-31 15:35:04 +0200

    Use ctoot dev instead of testing

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class Pkg(ConanFile):
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
     exports_sources = "src/*"
-    requires = ("ctoot/0.1@izmar/testing", "moduru/0.1@izmar/dev", "rapidjson/1.1.0@bincrafters/stable", "Catch2/2.7.0@catchorg/stable")
+    requires = ("ctoot/0.1@izmar/dev", "moduru/0.1@izmar/dev", "rapidjson/1.1.0@bincrafters/stable", "Catch2/2.7.0@catchorg/stable")
 
     def build(self):
         cmake = CMake(self)
