8d6bde0d9176ed692bce404ed89881a7f372099e
Izmar Verhage <izmaelverhage@gmail.com>
2020-04-18 14:13:34 +0200

    Don't build 32bit debug

--- rebuild32.bat
+++ rebuild32.bat
@@ -6,5 +6,3 @@ cd C:\Users\Izmar\git\mpc\build\32bit
 conan install -s build_type=Debug -s arch=x86 -g cmake_multi ../../ mpc/0.1@izmar/dev --build missing
 conan install -s build_type=Release -s arch=x86 -g cmake_multi ../../ mpc/0.1@izmar/dev --build missing
 conan build ../../
-conan install -s build_type=Debug -s arch=x86 -g cmake_multi ../../ mpc/0.1@izmar/dev
-conan build ../../
