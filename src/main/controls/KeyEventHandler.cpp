2ac76c082a47b29063b8367686c7a9c5bfa65860
Izmar Verhage <izmaelverhage@gmail.com>
2021-01-27 20:08:37 +0100

    Comment some debug logs

--- src/main/controls/KeyEventHandler.cpp
+++ src/main/controls/KeyEventHandler.cpp
@@ -35,7 +35,7 @@ KeyEventHandler::KeyEventHandler(mpc::Mpc& mpc)
 
 void KeyEventHandler::handle(const KeyEvent& keyEvent)
 {
-    MLOG("KeyEventHandler::handle keyEvent.rawKeyCode " + to_string(keyEvent.rawKeyCode) + string(keyEvent.keyDown ? " down" : " up"));
+    //MLOG("KeyEventHandler::handle keyEvent.rawKeyCode " + to_string(keyEvent.rawKeyCode) + string(keyEvent.keyDown ? " down" : " up"));
 
     auto screen = mpc.screens->get<VmpcKeyboardScreen>("vmpc-keyboard");
     auto kbMapping = mpc.getControls().lock()->getKbMapping().lock();
@@ -103,7 +103,7 @@ void KeyEventHandler::handle(const KeyEvent& keyEvent)
     
     auto label = kbMapping->getLabelFromKeyCode(keyEvent.rawKeyCode);
     
-    MLOG("Label associated with key code: " + label);
+    //MLOG("Label associated with key code: " + label);
     
     auto hardwareComponent = mpc.getHardware().lock()->getComponentByLabel(label).lock();
     
--- src/main/sequencer/FrameSeq.cpp
+++ src/main/sequencer/FrameSeq.cpp
@@ -33,7 +33,7 @@ FrameSeq::FrameSeq(mpc::Mpc& mpc)
 }
 
 void FrameSeq::start(float sampleRate) {
-	MLOG("frameSeq starting with sampleRate " + to_string(sampleRate));
+	//MLOG("frameSeq starting with sampleRate " + to_string(sampleRate));
 
 	if (running)
 		return;
