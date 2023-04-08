145cf242cfaf223b141ff453e40ed4fc4ffc4565
Izmar Verhage <izmaelverhage@gmail.com>
2023-04-08 11:29:49 -0500

    Remove useless try-catch

--- src/main/engine/audio/core/AudioProcessChain.cpp
+++ src/main/engine/audio/core/AudioProcessChain.cpp
@@ -29,25 +29,13 @@ void AudioProcessChain::open()
 	}
 }
 
-int AudioProcessChain::processAudio(mpc::engine::audio::core::AudioBuffer* buffer, int nFrames)
+int AudioProcessChain::processAudio(AudioBuffer* buffer, int nFrames)
 {
-	for (auto& p : processes) {
-		try {
-			if (p) {
-				p->processAudio(buffer, nFrames);
-			}
-		}
-		catch (const exception& e) {
-			try {
-				p->close();
-			}
-			catch (const exception& e2) {
-				e2.what();
-			}
-			p.reset();
-			string error = "DISABLED AudioProcess in " + getName() + " due to: " + e.what();
-		}
+	for (auto& p : processes)
+    {
+        p->processAudio(buffer, nFrames);
 	}
+
 	return AUDIO_OK;
 }
 
