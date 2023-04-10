f369f29d35e38aa9804c182364026256558d018a
Izmar Verhage <izmaelverhage@gmail.com>
2023-04-10 14:07:18 -0500

    Fix renaming

--- src/main/disk/AkaiFileRenamer.cpp
+++ src/main/disk/AkaiFileRenamer.cpp
@@ -16,11 +16,13 @@ void AkaiFileRenamer::renameFilesInDirectory(fs::path p)
 
     auto tempRoot = fs::temp_directory_path() / "VMPC2000XL";
 
-    if (!fs::exists(tempRoot))
+    if (fs::exists(tempRoot))
     {
-        auto tempRootWasCreated = fs::create_directory(tempRoot);
-        assert(tempRootWasCreated);
+        fs::remove_all(tempRoot);
     }
+
+    auto tempRootWasCreated = fs::create_directory(tempRoot);
+    assert(tempRootWasCreated);
 
     std::vector<std::string> existingNames;
 
@@ -42,7 +44,7 @@ void AkaiFileRenamer::renameFilesInDirectory(fs::path p)
         if (akaiName != e.path().filename().string())
         {
             existingNames.push_back(akaiName);
-            fs::rename(e, tempRoot / e.path().filename());
+            fs::rename(e, tempRoot / akaiName);
         }
         else
         {
