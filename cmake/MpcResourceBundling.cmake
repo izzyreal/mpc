ad343993239c615bf65bc2a2a8b51437ac397b1c
Izmar Verhage <izmaelverhage@gmail.com>
2024-03-03 14:46:59 +0100

    Move test resources to dedicated directory

--- cmake/MpcResourceBundling.cmake
+++ cmake/MpcResourceBundling.cmake
@@ -1,17 +1,17 @@
 include(cmake/CMakeRC.cmake)
 
 set(_mpc_resources_root ${CMAKE_CURRENT_SOURCE_DIR}/resources)
+set(_mpc_test_resources_root ${CMAKE_CURRENT_SOURCE_DIR}/test-resources)
 
 function(_bundle_resources)
 
   file(GLOB_RECURSE MPC_RESOURCES "${_mpc_resources_root}/*")
-  list(FILTER MPC_RESOURCES EXCLUDE REGEX "${_mpc_resources_root}/test/.*")
   list(FILTER MPC_RESOURCES EXCLUDE REGEX "\\.DS_Store$")
 
   if (APPLE)
     foreach(RESOURCE ${MPC_RESOURCES})
       get_filename_component(SOURCE_DIR "${RESOURCE}" DIRECTORY)
-      string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}/resources" "" RELATIVE_DIR "${SOURCE_DIR}")
+      string(REPLACE "${_mpc_resources_root}" "" RELATIVE_DIR "${SOURCE_DIR}")
       set_source_files_properties(${RESOURCE} PROPERTIES MACOSX_PACKAGE_LOCATION "Resources${RELATIVE_DIR}")
     endforeach()
     target_sources(mpc-tests PRIVATE ${MPC_RESOURCES})
@@ -27,14 +27,15 @@ function(_bundle_resources)
     target_link_libraries(mpc mpc::rc)
   endif()
 
-  file(GLOB_RECURSE MPC_TEST_RESOURCES "${CMAKE_CURRENT_SOURCE_DIR}/resources/test/*")
+  file(GLOB_RECURSE MPC_TEST_RESOURCES "${_mpc_test_resources_root}/*")
   list(FILTER MPC_TEST_RESOURCES EXCLUDE REGEX "\\.DS_Store$")
 
   cmrc_add_resource_library(
           mpc_test_resources
           ALIAS mpctest::rc
           NAMESPACE mpctest
-          WHENCE ${_mpc_resources_root}
+          WHENCE ${_mpc_test_resources_root}
+          PREFIX test
           ${MPC_TEST_RESOURCES}
   )
 
