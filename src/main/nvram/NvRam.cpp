db50df7f9a95164276e40ea3fa46e2de3d3ea981
Izmar Verhage <izmaelverhage@gmail.com>
2024-10-29 14:26:42 +0100

    Revert "Load nvram.vmp only if it has expected size #254"
    This reverts commit 417f4d352858068be971e76dc024367b09dffbb1.

--- src/main/nvram/NvRam.cpp
+++ src/main/nvram/NvRam.cpp
@@ -14,7 +14,6 @@
 #include <hardware/Hardware.hpp>
 #include <hardware/HwSlider.hpp>
 
-#include <file/all/AllParser.hpp>
 #include <file/all/Defaults.hpp>
 
 using namespace mpc::nvram;
@@ -25,8 +24,7 @@ void NvRam::loadUserScreenValues(mpc::Mpc& mpc)
 {
   const auto path = mpc.paths->configPath() / "nvram.vmp";
 
-  if (!fs::exists(path) ||
-      fs::file_size(path) != mpc::file::all::AllParser::DEFAULTS_LENGTH)
+  if (!fs::exists(path))
   {
       return;
   }
