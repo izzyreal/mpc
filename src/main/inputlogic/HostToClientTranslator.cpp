6b4836045974f390744779e8ebdfacdf9658b459
Izmar Verhage <izmaelverhage@gmail.com>
2025-10-10 12:28:28 -0500

    Translate data wheel events

--- src/main/inputlogic/HostToClientTranslator.cpp
+++ src/main/inputlogic/HostToClientTranslator.cpp
@@ -27,52 +27,88 @@ ClientInput HostToClientTranslator::translate(const HostInputEvent& hostEvent) {
         }
         break;
     }
+
     case HostInputEvent::MOUSE: {
         const auto& mouse = std::get<MouseEvent>(hostEvent.payload);
+
         switch (mouse.guiElement) {
         case MouseEvent::PAD1:
             clientEvent.index = 0;
-            clientEvent.type = (mouse.type == MouseEvent::DOWN) ? ClientInput::Type::PadPress : (mouse.type == MouseEvent::UP) ? ClientInput::Type::PadRelease : ClientInput::Type::PadAftertouch;
-            if (mouse.type == MouseEvent::DOWN || mouse.type == MouseEvent::UP || mouse.type == MouseEvent::MOVE) {
+            clientEvent.type = (mouse.type == MouseEvent::DOWN)
+                                   ? ClientInput::Type::PadPress
+                                   : (mouse.type == MouseEvent::UP)
+                                         ? ClientInput::Type::PadRelease
+                                         : ClientInput::Type::PadAftertouch;
+            if (mouse.type == MouseEvent::DOWN || mouse.type == MouseEvent::UP || mouse.type == MouseEvent::MOVE)
                 clientEvent.value = static_cast<int>((1.f - mouse.normY) * 127.f);
-            }
             break;
+
         case MouseEvent::PAD2:
             clientEvent.index = 1;
-            clientEvent.type = (mouse.type == MouseEvent::DOWN) ? ClientInput::Type::PadPress : (mouse.type == MouseEvent::UP) ? ClientInput::Type::PadRelease : ClientInput::Type::PadAftertouch;
-            if (mouse.type == MouseEvent::DOWN || mouse.type == MouseEvent::UP || mouse.type == MouseEvent::MOVE) {
+            clientEvent.type = (mouse.type == MouseEvent::DOWN)
+                                   ? ClientInput::Type::PadPress
+                                   : (mouse.type == MouseEvent::UP)
+                                         ? ClientInput::Type::PadRelease
+                                         : ClientInput::Type::PadAftertouch;
+            if (mouse.type == MouseEvent::DOWN || mouse.type == MouseEvent::UP || mouse.type == MouseEvent::MOVE)
                 clientEvent.value = static_cast<int>((1.f - mouse.normY) * 127.f);
-            }
             break;
+
         case MouseEvent::PAD3:
             clientEvent.index = 2;
-            clientEvent.type = (mouse.type == MouseEvent::DOWN) ? ClientInput::Type::PadPress : (mouse.type == MouseEvent::UP) ? ClientInput::Type::PadRelease : ClientInput::Type::PadAftertouch;
-            if (mouse.type == MouseEvent::DOWN || mouse.type == MouseEvent::UP || mouse.type == MouseEvent::MOVE) {
+            clientEvent.type = (mouse.type == MouseEvent::DOWN)
+                                   ? ClientInput::Type::PadPress
+                                   : (mouse.type == MouseEvent::UP)
+                                         ? ClientInput::Type::PadRelease
+                                         : ClientInput::Type::PadAftertouch;
+            if (mouse.type == MouseEvent::DOWN || mouse.type == MouseEvent::UP || mouse.type == MouseEvent::MOVE)
                 clientEvent.value = static_cast<int>((1.f - mouse.normY) * 127.f);
-            }
             break;
+
         case MouseEvent::PLAY_BUTTON:
             clientEvent.label = "play";
-            clientEvent.type = (mouse.type == MouseEvent::DOWN) ? ClientInput::Type::ButtonPress : ClientInput::Type::ButtonRelease;
+            clientEvent.type = (mouse.type == MouseEvent::DOWN)
+                                   ? ClientInput::Type::ButtonPress
+                                   : ClientInput::Type::ButtonRelease;
             break;
+
         case MouseEvent::STOP_BUTTON:
             clientEvent.label = "stop";
-            clientEvent.type = (mouse.type == MouseEvent::DOWN) ? ClientInput::Type::ButtonPress : ClientInput::Type::ButtonRelease;
+            clientEvent.type = (mouse.type == MouseEvent::DOWN)
+                                   ? ClientInput::Type::ButtonPress
+                                   : ClientInput::Type::ButtonRelease;
             break;
+
+        case MouseEvent::DATA_WHEEL:
+            // --- Handle all DataWheel GUI events here ---
+            clientEvent.type = ClientInput::Type::DataWheelTurn;
+            if (mouse.type == MouseEvent::WHEEL)
+                clientEvent.value = static_cast<int>(mouse.wheelDelta);
+            else if (mouse.type == MouseEvent::MOVE || mouse.type == MouseEvent::DRAG)
+                clientEvent.value = static_cast<int>(mouse.deltaY);
+            else
+                clientEvent.value = 0;
+            break;
+
         case MouseEvent::NONE:
             throw std::invalid_argument("guiElement must be provided\n");
         }
-        if (mouse.type == MouseEvent::WHEEL) {
+
+        // Wheel fallback (for other GUI elements)
+        if (mouse.guiElement != MouseEvent::DATA_WHEEL && mouse.type == MouseEvent::WHEEL) {
             clientEvent.type = ClientInput::Type::DataWheelTurn;
             clientEvent.value = static_cast<int>(mouse.wheelDelta);
         }
+
         break;
     }
+
     case HostInputEvent::KEYBOARD: {
         const auto& key = std::get<KeyEvent>(hostEvent.payload);
-        if (key.rawKeyCode == 32) {
+        if (key.rawKeyCode == 32) { // Spacebar
             clientEvent.label = "play";
-            clientEvent.type = key.keyDown ? ClientInput::Type::ButtonPress : ClientInput::Type::ButtonRelease;
+            clientEvent.type = key.keyDown ? ClientInput::Type::ButtonPress
+                                           : ClientInput::Type::ButtonRelease;
         }
         break;
     }
@@ -80,3 +116,4 @@ ClientInput HostToClientTranslator::translate(const HostInputEvent& hostEvent) {
 
     return clientEvent;
 }
+
