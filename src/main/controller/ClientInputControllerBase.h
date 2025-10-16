ac40de862141348fca4917571762cf9206e23213
Izmar Verhage <izmaelverhage@gmail.com>
2025-10-15 20:29:15 -0500

    Track physical pad pushes

--- src/main/controller/ClientInputControllerBase.h
+++ src/main/controller/ClientInputControllerBase.h
@@ -9,11 +9,17 @@
 #include "mpc_fs.hpp"
 
 #include "controls/KbMapping.hpp"
+#include <cassert>
 
 namespace mpc::controller {
 
 class ClientInputControllerBase {
     public:
+        struct PhysicalPadPress {
+            int bankIndex;
+            std::string screenName;
+        };
+
         explicit ClientInputControllerBase(const fs::path keyboardMappingConfigDirectory)
         {
             kbMapping = std::make_shared<mpc::controls::KbMapping>(keyboardMappingConfigDirectory);
@@ -44,8 +50,26 @@ class ClientInputControllerBase {
 
         std::shared_ptr<mpc::controls::KbMapping> getKbMapping() { return kbMapping; }
 
+        void registerPhysicalPadPush(const int padIndex, const int bankIndex, const std::string screenName)
+        {
+            assert(physicalPadPresses.count(padIndex) == 0);
+            physicalPadPresses[padIndex] = { bankIndex, screenName };
+        }
+
+        PhysicalPadPress registerPhysicalPadRelease(const int padIndex)
+        {
+            assert(physicalPadPresses.count(padIndex) == 1);
+            const auto result = physicalPadPresses[padIndex];
+            physicalPadPresses.erase(padIndex);
+            return result;
+        }
+
     private:
         std::shared_ptr<mpc::controls::KbMapping> kbMapping;
+        //
+        // Maps physical pad index 0 - 15 to info at the time the pad was pushed.
+        std::unordered_map<int, PhysicalPadPress> physicalPadPresses;
+
 };
 
 } // namespace mpc::controller
