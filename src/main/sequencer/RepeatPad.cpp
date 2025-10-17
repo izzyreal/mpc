161418c5798c7411cc4d2b22219e8af237ffe5fa
Izmar Verhage <izmaelverhage@gmail.com>
2025-10-17 11:16:42 -0500

    Fix note repeat

--- src/main/sequencer/RepeatPad.cpp
+++ src/main/sequencer/RepeatPad.cpp
@@ -27,6 +27,8 @@ void RepeatPad::process(mpc::Mpc& mpc,
         return;
     }
 
+    using PadPressSource = mpc::sampler::Program::PadPressSource;
+
     auto sequencer = mpc.getSequencer();
     auto track = sequencer->getActiveTrack();
 
@@ -43,125 +45,125 @@ void RepeatPad::process(mpc::Mpc& mpc,
     auto assign16LevelsScreen = mpc.screens->get<Assign16LevelsScreen>("assign-16-levels");
     auto note = assign16LevelsScreen->getNote();
 
-    for (int padIndexWithBank = 0; padIndexWithBank < 64; ++padIndexWithBank)
+    for (int physicalPadIndex = 0; physicalPadIndex < 16; ++physicalPadIndex)
     {
-        if (!mpc.inputController->isPhysicallyPressed(padIndexWithBank % 16, mpc.getBank()))
+        for (int bankIndex = 0; bankIndex < 4; ++bankIndex)
         {
-            continue;
-        }
-
-        if (!sixteenLevels && program)
-        {
-            note = program->getNoteFromPad(padIndexWithBank);
-        }
-
-        auto noteEvent = std::make_shared<NoteOnEvent>(note);
-        noteEvent->setTick(static_cast<int>(tickPosition));
-        const bool isSliderNote = program && program->getSlider()->getNote() == note;
-
-        if (program && isSliderNote)
-        {
-            auto hardwareSlider = mpc.getHardware()->getSlider();
-            auto programSlider = program->getSlider();
-
-            Util::SliderNoteVariationContext sliderNoteVariationContext {
-                hardwareSlider->getValueAs<int>(),
-                programSlider->getNote(),
-                programSlider->getParameter(),
-                programSlider->getTuneLowRange(),
-                programSlider->getTuneHighRange(),
-                programSlider->getDecayLowRange(),
-                programSlider->getDecayHighRange(),
-                programSlider->getAttackLowRange(),
-                programSlider->getAttackHighRange(),
-                programSlider->getFilterLowRange(),
-                programSlider->getFilterHighRange()
-            };
-
-            mpc::Util::setSliderNoteVariationParameters(sliderNoteVariationContext, noteEvent);
-        }
-
-        const int hardwarePadIndex = padIndexWithBank % 16;
-        const auto hardwarePad = mpc.getHardware()->getPad(hardwarePadIndex);
-
-        if (sixteenLevels)
-        {
-            Util::SixteenLevelsContext sixteenLevelsContext {
-                sixteenLevels,
-                assign16LevelsScreen->getType(),
-                assign16LevelsScreen->getOriginalKeyPad(),
-                assign16LevelsScreen->getNote(),
-                assign16LevelsScreen->getParameter(),
-                hardwarePadIndex
-            };
-
-            noteEvent->setVelocity(127);
-            mpc::Util::set16LevelsValues(sixteenLevelsContext, noteEvent);
-            note = noteEvent->getNote();
-        }
-        else
-        {
-            assert(hardwarePad->getPressure().has_value() || hardwarePad->getVelocity().has_value());
-            const int velocityToUseIfNotFullLevel = hardwarePad->getPressure().value_or(hardwarePad->getVelocity().value());
-            noteEvent->setVelocity(fullLevel ? 127 : velocityToUseIfNotFullLevel);
-        }
-
-        noteEvent->setDuration(durationTicks);
-
-        noteEvent->getNoteOff()->setTick(static_cast<int>(tickPosition) + durationTicks);
-
-        const auto velocityBeforeTrackVelocityRatioApplied = noteEvent->getVelocity();
-
-        noteEvent->setVelocity(noteEvent->getVelocity() * (track->getVelocityRatio() * 0.01));
-
-        const auto durationFrames = static_cast<int>(durationTicks == -1 ? -1 :
-                                               SeqUtil::ticksToFrames(durationTicks, tempo, sampleRate));
-
-        if (track->getBus() > 0 && note != 34)
-        {
-            const auto noteParameters = program->getNoteParameters(note);
-            const auto sound = mpc.getSampler()->getSound(noteParameters->getSoundIndex());
-            auto voiceOverlap = (sound && sound->isLoopEnabled()) ? 2 : noteParameters->getVoiceOverlap();
-
-            mpc.getDrum(track->getBus() - 1).mpcNoteOn(
-                note, noteEvent->getVelocity(), noteEvent->getVariationType(), noteEvent->getVariationValue(), eventFrameOffset, true, -1,
-                voiceOverlap == 2 ? durationFrames : -1);
-        }
-
-        if (track->getDeviceIndex() > 0)
-        {
-            const auto noteOnMsg = noteEvent->createShortMessage((track->getDeviceIndex() - 1) % 16);
-            noteOnMsg->bufferPos = eventFrameOffset;
-            mpc.getMidiOutput()->enqueueMessageOutputA(noteOnMsg);
-        }
-
-        if (sequencer->isRecordingOrOverdubbing())
-        {
-            noteEvent->setVelocity(velocityBeforeTrackVelocityRatioApplied);
-            track->insertEventWhileRetainingSort(noteEvent);
-        }
-
-        mpc.getAudioMidiServices()->getFrameSequencer()->enqueueEventAfterNFrames(
-                [&mpc, track, note, noteEvent, tickPosition, sixteenLevels, program, padIndexWithBank]
-                (const int bufferOffset){
-            if (track->getBus() > 0)
+            if (!mpc.inputController->isPhysicallyPressed(physicalPadIndex, bankIndex))
             {
-                mpc.getDrum(track->getBus() - 1).mpcNoteOff(note, bufferOffset, tickPosition);
-                // Implement ghost pad glow
-                //const auto padToNotify = sixteenLevels ? hardware->getPad(program->getPadIndexFromNote(note)) : padIndexWithBank;
-                // Notify the pad of the note off/pad release
+                continue;
+            }
+
+            const int programPadIndex = physicalPadIndex + bankIndex * 16;
+
+            if (!sixteenLevels && program)
+            {
+                note = program->getNoteFromPad(programPadIndex);
+            }
+
+            auto noteEvent = std::make_shared<NoteOnEvent>(note);
+            noteEvent->setTick(static_cast<int>(tickPosition));
+            const bool isSliderNote = program && program->getSlider()->getNote() == note;
+
+            if (program && isSliderNote)
+            {
+                auto hardwareSlider = mpc.getHardware()->getSlider();
+                auto programSlider = program->getSlider();
+
+                Util::SliderNoteVariationContext sliderNoteVariationContext {
+                    hardwareSlider->getValueAs<int>(),
+                    programSlider->getNote(),
+                    programSlider->getParameter(),
+                    programSlider->getTuneLowRange(),
+                    programSlider->getTuneHighRange(),
+                    programSlider->getDecayLowRange(),
+                    programSlider->getDecayHighRange(),
+                    programSlider->getAttackLowRange(),
+                    programSlider->getAttackHighRange(),
+                    programSlider->getFilterLowRange(),
+                    programSlider->getFilterHighRange()
+                };
+
+                mpc::Util::setSliderNoteVariationParameters(sliderNoteVariationContext, noteEvent);
+            }
+
+            if (sixteenLevels)
+            {
+                Util::SixteenLevelsContext sixteenLevelsContext {
+                    sixteenLevels,
+                    assign16LevelsScreen->getType(),
+                    assign16LevelsScreen->getOriginalKeyPad(),
+                    assign16LevelsScreen->getNote(),
+                    assign16LevelsScreen->getParameter(),
+                    physicalPadIndex
+                };
+
+                noteEvent->setVelocity(127);
+                mpc::Util::set16LevelsValues(sixteenLevelsContext, noteEvent);
+                note = noteEvent->getNote();
+            }
+            else
+            {
+                const auto hardwarePad = mpc.getHardware()->getPad(physicalPadIndex);
+                assert(hardwarePad->getPressure().has_value() || hardwarePad->getVelocity().has_value());
+                const int velocityToUseIfNotFullLevel = hardwarePad->getPressure().value_or(hardwarePad->getVelocity().value());
+                noteEvent->setVelocity(fullLevel ? 127 : velocityToUseIfNotFullLevel);
+            }
+
+            noteEvent->setDuration(durationTicks);
+
+            noteEvent->getNoteOff()->setTick(static_cast<int>(tickPosition) + durationTicks);
+
+            const auto velocityBeforeTrackVelocityRatioApplied = noteEvent->getVelocity();
+
+            noteEvent->setVelocity(noteEvent->getVelocity() * (track->getVelocityRatio() * 0.01));
+
+            const auto durationFrames = static_cast<int>(durationTicks == -1 ? -1 :
+                                                   SeqUtil::ticksToFrames(durationTicks, tempo, sampleRate));
+
+            if (track->getBus() > 0 && note != 34)
+            {
+                const auto noteParameters = program->getNoteParameters(note);
+                const auto sound = mpc.getSampler()->getSound(noteParameters->getSoundIndex());
+                auto voiceOverlap = (sound && sound->isLoopEnabled()) ? 2 : noteParameters->getVoiceOverlap();
+
+                mpc.getDrum(track->getBus() - 1).mpcNoteOn(
+                    note, noteEvent->getVelocity(), noteEvent->getVariationType(), noteEvent->getVariationValue(), eventFrameOffset, true, -1,
+                    voiceOverlap == 2 ? durationFrames : -1);
+
+                program->registerPadPress(programPadIndex, PadPressSource::NON_PHYSICAL);
             }
 
             if (track->getDeviceIndex() > 0)
             {
-                const auto noteOffMsg = noteEvent->getNoteOff()->createShortMessage((track->getDeviceIndex() - 1) % 16);
-                noteOffMsg->bufferPos = bufferOffset;
-                mpc.getMidiOutput()->enqueueMessageOutputA(noteOffMsg);
+                const auto noteOnMsg = noteEvent->createShortMessage((track->getDeviceIndex() - 1) % 16);
+                noteOnMsg->bufferPos = eventFrameOffset;
+                mpc.getMidiOutput()->enqueueMessageOutputA(noteOnMsg);
             }
-        }, durationFrames - 1);
 
-        //const auto padToNotify = sixteenLevels ? hardware->getPad(program->getPadIndexFromNote(note)) : padIndexWithBank;
-        // Notify the pad of the note on/pad push, with the right velocity
+            if (sequencer->isRecordingOrOverdubbing())
+            {
+                noteEvent->setVelocity(velocityBeforeTrackVelocityRatioApplied);
+                track->insertEventWhileRetainingSort(noteEvent);
+            }
+
+            mpc.getAudioMidiServices()->getFrameSequencer()->enqueueEventAfterNFrames(
+                    [&mpc, track, note, noteEvent, tickPosition, program, programPadIndex]
+                    (const int bufferOffset){
+                if (track->getBus() > 0)
+                {
+                    mpc.getDrum(track->getBus() - 1).mpcNoteOff(note, bufferOffset, tickPosition);
+                    program->registerPadRelease(programPadIndex, PadPressSource::NON_PHYSICAL);
+                }
+
+                if (track->getDeviceIndex() > 0)
+                {
+                    const auto noteOffMsg = noteEvent->getNoteOff()->createShortMessage((track->getDeviceIndex() - 1) % 16);
+                    noteOffMsg->bufferPos = bufferOffset;
+                    mpc.getMidiOutput()->enqueueMessageOutputA(noteOffMsg);
+                }
+            }, durationFrames - 1);
+        }
     }
 }
 
