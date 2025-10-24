3e3f5f99795c02430a007f8f50f5e788fd69bade
Izmar Verhage <izmaelverhage@gmail.com>
2025-10-24 09:42:11 -0500

    Log MIDI events to MLOG

---
+++ src/main/input/MidiEvent.cpp
@@ -0,0 +1,53 @@
+#include "input/MidiEvent.hpp"
+
+#include "Logger.hpp"
+
+#include <cstdio>
+#include <string>
+
+using namespace mpc::input;
+
+void MidiEvent::printInfo() const noexcept
+{
+    static const char* typeNames[] = {
+        "NOTE_ON", "NOTE_OFF", "CONTROLLER", "AFTERTOUCH",
+        "CHANNEL_PRESSURE", "MIDI_CLOCK", "MIDI_START",
+        "MIDI_STOP", "MIDI_CONTINUE", "PROGRAM_CHANGE", "PITCH_WHEEL"
+    };
+
+    std::string msg = "MIDI Event: " + std::string(typeNames[messageType]) +
+        " | Ch: " + std::to_string(channel) +
+        " | Offset: " + std::to_string(bufferOffset) + " | ";
+
+    switch (messageType) {
+        case NOTE_ON:
+        case NOTE_OFF:
+            msg += "Note: " + std::to_string(getNoteNumber()) +
+                " | Vel: " + std::to_string(getVelocity());
+            break;
+        case CONTROLLER:
+            msg += "CC#: " + std::to_string(getControllerNumber()) +
+                " | Value: " + std::to_string(getControllerValue());
+            break;
+        case AFTERTOUCH:
+            msg += "Note: " + std::to_string(getAftertouchNote()) +
+                " | Pressure: " + std::to_string(getAftertouchValue());
+            break;
+        case CHANNEL_PRESSURE:
+            msg += "Pressure: " + std::to_string(getChannelPressure());
+            break;
+        case PROGRAM_CHANGE:
+            msg += "Program: " + std::to_string(getProgramNumber());
+            break;
+        case PITCH_WHEEL:
+            msg += "Pitch: " + std::to_string(getPitchWheelValue());
+            break;
+        default:
+            msg += "No data bytes";
+            break;
+    }
+
+    std::printf("%s\n", msg.c_str());
+    MLOG(msg);
+}
+
--- src/main/input/MidiEvent.hpp
+++ src/main/input/MidiEvent.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cstdint>
-#include <cstdio>
 
 namespace mpc::input {
     struct MidiEvent {
@@ -19,25 +18,27 @@ namespace mpc::input {
             PITCH_WHEEL
         };
 
-        private:
+    private:
         MessageType messageType{};
         uint8_t channel{};
         uint8_t data1{};
         uint8_t data2{};
         double bufferOffset{};
 
-        public:
+    public:
         MidiEvent() = default;
         MidiEvent(MessageType type, int d1 = 0, int d2 = 0)
             : messageType(type), data1(static_cast<uint8_t>(d1)), data2(static_cast<uint8_t>(d2)) {}
 
         MessageType getMessageType() const noexcept { return messageType; }
         void setMessageType(MessageType type) noexcept { messageType = type; }
-        // 0 - 15
-        void setChannel(const int c) noexcept { channel = c; }
-        void setBufferOffset(const double b) noexcept { bufferOffset = b; }
 
-        // --- Logical getters/setters depending on message type ---
+        void setChannel(const int c) noexcept { channel = static_cast<uint8_t>(c); }
+        int getChannel() const noexcept { return static_cast<int>(channel); }
+
+        void setBufferOffset(const double b) noexcept { bufferOffset = b; }
+        double getBufferOffset() const noexcept { return bufferOffset; }
+
         int getNoteNumber() const noexcept { return static_cast<int>(data1); }
         void setNoteNumber(int n) noexcept { data1 = static_cast<uint8_t>(n); }
 
@@ -62,54 +63,16 @@ namespace mpc::input {
         int getProgramNumber() const noexcept { return static_cast<int>(data1); }
         void setProgramNumber(int n) noexcept { data1 = static_cast<uint8_t>(n); }
 
-        int getPitchWheelValue() const noexcept
-        {
+        int getPitchWheelValue() const noexcept {
             return static_cast<int>((data2 << 7) | data1) - 8192;
         }
-        
-        void setPitchWheelValue(int v) noexcept
-        {
+        void setPitchWheelValue(int v) noexcept {
             v += 8192;
             data1 = static_cast<uint8_t>(v & 0x7F);
             data2 = static_cast<uint8_t>((v >> 7) & 0x7F);
         }
 
-        void printInfo() const noexcept
-        {
-            static const char* typeNames[] = {
-                "NOTE_ON", "NOTE_OFF", "CONTROLLER", "AFTERTOUCH",
-                "CHANNEL_PRESSURE", "MIDI_CLOCK", "MIDI_START",
-                "MIDI_STOP", "MIDI_CONTINUE", "PROGRAM_CHANGE", "PITCH_WHEEL"
-            };
-
-            std::printf("MIDI Event: %s | Ch: %d | Offset: %.3f | ",
-                        typeNames[messageType], channel, bufferOffset);
-
-            switch (messageType)
-            {
-                case NOTE_ON:
-                case NOTE_OFF:
-                    std::printf("Note: %d | Vel: %d\n", getNoteNumber(), getVelocity());
-                    break;
-                case CONTROLLER:
-                    std::printf("CC#: %d | Value: %d\n", getControllerNumber(), getControllerValue());
-                    break;
-                case AFTERTOUCH:
-                    std::printf("Note: %d | Pressure: %d\n", getAftertouchNote(), getAftertouchValue());
-                    break;
-                case CHANNEL_PRESSURE:
-                    std::printf("Pressure: %d\n", getChannelPressure());
-                    break;
-                case PROGRAM_CHANGE:
-                    std::printf("Program: %d\n", getProgramNumber());
-                    break;
-                case PITCH_WHEEL:
-                    std::printf("Pitch: %d\n", getPitchWheelValue());
-                    break;
-                default:
-                    std::printf("No data bytes\n");
-                    break;
-            }
-        }
+        void printInfo() const noexcept;
     };
 } // namespace mpc::input
+
