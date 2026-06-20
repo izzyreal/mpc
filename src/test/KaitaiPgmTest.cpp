59472937e7ba66aa5b87b49f42c739b20fb6696b
Izmar <3707432+izzyreal@users.noreply.github.com>
2026-06-20 18:26:32 +0200

    Broaden PGM handwritten-path parity coverage

--- src/test/KaitaiPgmTest.cpp
+++ src/test/KaitaiPgmTest.cpp
@@ -16,6 +16,7 @@
 #include "file/pgmreader/PRSlider.hpp"
 #include "file/pgmwriter/PgmWriter.hpp"
 #include "performance/PerformanceManager.hpp"
+#include "sampler/Pad.hpp"
 #include "sampler/PgmSlider.hpp"
 #include "sampler/Program.hpp"
 #include "sampler/Sampler.hpp"
@@ -33,6 +34,144 @@
 CMRC_DECLARE(mpctest);
 
 namespace {
+
+std::vector<char> rewriteProgram1WithMutations(
+    const std::function<void(mpc2000xl_pgm_t&)>& mutate)
+{
+    auto fs = cmrc::mpctest::get_filesystem();
+    auto file = fs.open("test/ProgramLoading/program1/PROGRAM1.PGM");
+    const std::string originalBytes(
+        std::string_view(file.begin(), file.end() - file.begin())
+    );
+
+    std::stringstream parseStream(
+        originalBytes,
+        std::ios::in | std::ios::out | std::ios::binary
+    );
+    kaitai::kstream parseIo(&parseStream);
+    mpc2000xl_pgm_t parsed(&parseIo);
+    parsed._read();
+
+    mutate(parsed);
+
+    std::stringstream writeStream(std::ios::in | std::ios::out | std::ios::binary);
+    kaitai::kstream writeIo(&writeStream);
+    parsed._set_io(&writeIo);
+    parsed._check();
+    parsed._write();
+
+    const auto rewrittenBytes = writeStream.str();
+    return std::vector<char>(rewrittenBytes.begin(), rewrittenBytes.end());
+}
+
+void applyBroadProgramMutation(mpc2000xl_pgm_t& parsed)
+{
+    auto* note35 = parsed.note_parameters()->at(0).get();
+    note35->set_sound_index(255);
+    note35->set_sound_generation_mode(mpc2000xl_pgm_t::SOUND_GENERATION_MODE_VEL_SW);
+    note35->set_velocity_range_lower(1);
+    note35->set_velocity_range_upper(127);
+    note35->set_also_play_use_note_1(41);
+    note35->set_also_play_use_note_2(42);
+    note35->set_voice_overlap_mode(mpc2000xl_pgm_t::VOICE_OVERLAP_MODE_NOTE_OFF);
+    note35->set_mute_assign_1(43);
+    note35->set_mute_assign_2(44);
+    note35->set_tune(-120);
+    note35->set_attack(99);
+    note35->set_decay(98);
+    note35->set_decay_mode(mpc2000xl_pgm_t::DECAY_MODE_START);
+    note35->set_cutoff(17);
+    note35->set_resonance(18);
+    note35->set_velocity_envelope_to_filter_attack(19);
+    note35->set_velocity_envelope_to_filter_decay(20);
+    note35->set_velocity_envelope_to_filter_amount(21);
+    note35->set_velocity_to_level(22);
+    note35->set_velocity_to_attack(23);
+    note35->set_velocity_to_start(24);
+    note35->set_velocity_to_cutoff(25);
+    note35->set_slider_parameter(mpc2000xl_pgm_t::SLIDER_PARAMETER_FILTER);
+    note35->set_velocity_to_pitch(26);
+
+    auto* mixer35 = parsed.pad_mixers()->at(0).get();
+    mixer35->set_fx_output(mpc2000xl_pgm_t::FX_OUTPUT_R2);
+    mixer35->set_volume(12);
+    mixer35->set_pan(13);
+    mixer35->set_volume_individual(14);
+    mixer35->set_output(2);
+    mixer35->set_effects_send_level(15);
+
+    parsed.slider()->set_note(52);
+    parsed.slider()->set_tune_low(-119);
+    parsed.slider()->set_tune_high(118);
+    parsed.slider()->set_decay_low(11);
+    parsed.slider()->set_decay_high(12);
+    parsed.slider()->set_attack_low(13);
+    parsed.slider()->set_attack_high(14);
+    parsed.slider()->set_filter_low(-15);
+    parsed.slider()->set_filter_high(16);
+
+    parsed.set_program_change(126);
+    parsed.pad_to_note_mapping()->at(0) = 50;
+}
+
+void requireBroadMutatedProgramState(
+    const std::shared_ptr<mpc::sampler::Program>& program,
+    const std::vector<std::string>& soundNames)
+{
+    REQUIRE(program != nullptr);
+    REQUIRE(soundNames.size() == 2U);
+    REQUIRE(soundNames[0] == std::string("sound1") + std::string(10, ' '));
+    REQUIRE(soundNames[1] == std::string("sound2") + std::string(10, ' '));
+
+    REQUIRE(program->getPad(0)->getNote() == 50);
+    REQUIRE(program->getMidiProgramChange() == 127);
+
+    const auto note35 = program->getNoteParameters(35);
+    REQUIRE(note35->getSoundIndex() == -1);
+    REQUIRE(note35->getSoundGenerationMode() == 2);
+    REQUIRE(note35->getVelocityRangeLower() == 1);
+    REQUIRE(note35->getVelocityRangeUpper() == 127);
+    REQUIRE(note35->getOptionalNoteA() == 41);
+    REQUIRE(note35->getOptionalNoteB() == 42);
+    REQUIRE(note35->getMuteAssignA() == 43);
+    REQUIRE(note35->getMuteAssignB() == 44);
+    REQUIRE(note35->getTune() == -120);
+    REQUIRE(note35->getAttack() == 99);
+    REQUIRE(note35->getDecay() == 98);
+    REQUIRE(note35->getDecayMode() == 1);
+    REQUIRE(note35->getFilterFrequency() == 17);
+    REQUIRE(note35->getFilterResonance() == 18);
+    REQUIRE(note35->getFilterAttack() == 19);
+    REQUIRE(note35->getFilterDecay() == 20);
+    REQUIRE(note35->getFilterEnvelopeAmount() == 21);
+    REQUIRE(note35->getVeloToLevel() == 22);
+    REQUIRE(note35->getVelocityToAttack() == 23);
+    REQUIRE(note35->getVelocityToStart() == 24);
+    REQUIRE(note35->getVelocityToFilterFrequency() == 25);
+    REQUIRE(note35->getSliderParameterNumber() == 3);
+    REQUIRE(note35->getVelocityToPitch() == 26);
+    REQUIRE(note35->getVoiceOverlapMode() == mpc::sampler::VoiceOverlapMode::NOTE_OFF);
+
+    const auto stereoMixer = note35->getStereoMixer();
+    const auto indivFxMixer = note35->getIndivFxMixer();
+    REQUIRE(stereoMixer->getLevel() == 12);
+    REQUIRE(stereoMixer->getPanning() == 13);
+    REQUIRE(indivFxMixer->getVolumeIndividualOut() == 14);
+    REQUIRE(indivFxMixer->getOutput() == 2);
+    REQUIRE(indivFxMixer->getFxPath() == 4);
+    REQUIRE(indivFxMixer->getFxSendLevel() == 15);
+
+    const auto slider = program->getSlider();
+    REQUIRE(slider->getNote() == 52);
+    REQUIRE(slider->getTuneLowRange() == -119);
+    REQUIRE(slider->getTuneHighRange() == 118);
+    REQUIRE(slider->getDecayLowRange() == 11);
+    REQUIRE(slider->getDecayHighRange() == 12);
+    REQUIRE(slider->getAttackLowRange() == 13);
+    REQUIRE(slider->getAttackHighRange() == 14);
+    REQUIRE(slider->getFilterLowRange() == -15);
+    REQUIRE(slider->getFilterHighRange() == 16);
+}
 
 void prepareProgramLoadingResources(mpc::Mpc& mpc)
 {
@@ -647,4 +786,38 @@ TEST_CASE("ProgramLoader loads mutated note tune", "[kaitai-pgm]")
 
     REQUIRE(program != nullptr);
     REQUIRE(program->getNoteParameters(35)->getTune() == -120);
+}
+
+TEST_CASE("PgmFileToProgramConverter loads broad mutated program semantics", "[kaitai-pgm]")
+{
+    const auto programBytes = rewriteProgram1WithMutations(applyBroadProgramMutation);
+
+    mpc::Mpc mpc;
+    mpc::TestMpc::initializeTestMpcWithoutIoServices(mpc);
+    auto pgmFile = loadProgram1FileFromBytes(mpc, programBytes);
+    auto program = mpc.getSampler()->createNewProgramAddFirstAvailableSlotAndThen({}).lock();
+    std::vector<std::string> soundNames;
+
+    auto result = mpc::disk::PgmFileToProgramConverter::loadFromFileAndConvert(
+        mpc, pgmFile, program, soundNames
+    );
+    REQUIRE(result);
+
+    mpc.getPerformanceManager().lock()->drainQueue();
+    requireBroadMutatedProgramState(program, soundNames);
+}
+
+TEST_CASE("ProgramLoader loads broad mutated program semantics", "[kaitai-pgm]")
+{
+    const auto programBytes = rewriteProgram1WithMutations(applyBroadProgramMutation);
+
+    mpc::Mpc mpc;
+    mpc::TestMpc::initializeTestMpcWithoutMidiServices(mpc);
+    const auto program = loadProgram1FromBytesWithoutRender(mpc, programBytes);
+    const std::vector<std::string> expectedSoundNames{
+        std::string("sound1") + std::string(10, ' '),
+        std::string("sound2") + std::string(10, ' ')
+    };
+
+    requireBroadMutatedProgramState(program, expectedSoundNames);
 }
