dcb1cc51546a20b8f18a77fb7eb4872c7644fd36
Izmar <3707432+izzyreal@users.noreply.github.com>
2026-06-20 17:27:05 +0200

    Add focused PGM reader and converter parity tests

--- src/test/KaitaiPgmTest.cpp
+++ src/test/KaitaiPgmTest.cpp
@@ -5,12 +5,15 @@
 #include "IntTypes.hpp"
 #include "disk/AbstractDisk.hpp"
 #include "disk/MpcFile.hpp"
+#include "disk/PgmFileToProgramConverter.hpp"
 #include "disk/ProgramLoader.hpp"
 #include "engine/IndivFxMixer.hpp"
 #include "engine/EngineHost.hpp"
 #include "engine/StereoMixer.hpp"
 #include "file/kaitai/PgmIo.hpp"
 #include "file/kaitai/generated/mpc2000xl_pgm.h"
+#include "file/pgmreader/ProgramFileReader.hpp"
+#include "file/pgmreader/PRSlider.hpp"
 #include "file/pgmwriter/PgmWriter.hpp"
 #include "performance/PerformanceManager.hpp"
 #include "sampler/PgmSlider.hpp"
@@ -94,6 +97,21 @@ std::shared_ptr<mpc::sampler::Program> loadProgram1FromBytes(
     (void)mpc::disk::ProgramLoader::loadProgram(mpc, pgmFile, program, 0);
     mpc.getEngineHost()->prepareProcessBlock(512);
     return program;
+}
+
+std::shared_ptr<mpc::disk::MpcFile> loadProgram1FileFromBytes(
+    mpc::Mpc& mpc,
+    const std::vector<char>& programBytes)
+{
+    prepareProgramLoadingResources(mpc);
+
+    auto disk = mpc.getDisk();
+    disk->moveForward("program1");
+    disk->initFiles();
+    auto pgmFile = disk->getFile("PROGRAM1.PGM");
+    auto mutableBytes = programBytes;
+    pgmFile->setFileData(mutableBytes);
+    return pgmFile;
 }
 
 void requireProgram1LoadedState(mpc::Mpc& mpc,
@@ -422,4 +440,120 @@ TEST_CASE("Kaitai MPC2000 PGM load preserves mixer effects send level", "[kaitai
 
     REQUIRE(program != nullptr);
     REQUIRE(program->getNoteParameters(36)->getIndivFxMixer()->getFxSendLevel() == 50);
+}
+
+TEST_CASE("PgmFileToProgramConverter loads upper-bound MIDI program change", "[kaitai-pgm]")
+{
+    auto fs = cmrc::mpctest::get_filesystem();
+    auto file = fs.open("test/ProgramLoading/program1/PROGRAM1.PGM");
+    const std::string originalBytes(
+        std::string_view(file.begin(), file.end() - file.begin())
+    );
+
+    std::stringstream parseStream(
+        originalBytes,
+        std::ios::in | std::ios::out | std::ios::binary
+    );
+    kaitai::kstream parseIo(&parseStream);
+    mpc2000xl_pgm_t parsed(&parseIo);
+    parsed._read();
+    parsed.set_program_change(127);
+
+    std::stringstream writeStream(std::ios::in | std::ios::out | std::ios::binary);
+    kaitai::kstream writeIo(&writeStream);
+    parsed._set_io(&writeIo);
+    parsed._check();
+    parsed._write();
+
+    const auto rewrittenBytes = writeStream.str();
+    std::vector<char> programBytes(rewrittenBytes.begin(), rewrittenBytes.end());
+
+    mpc::Mpc mpc;
+    mpc::TestMpc::initializeTestMpcWithoutIoServices(mpc);
+    auto pgmFile = loadProgram1FileFromBytes(mpc, programBytes);
+    auto program = mpc.getSampler()->createNewProgramAddFirstAvailableSlotAndThen({}).lock();
+    std::vector<std::string> soundNames;
+
+    auto result = mpc::disk::PgmFileToProgramConverter::loadFromFileAndConvert(
+        mpc, pgmFile, program, soundNames
+    );
+    REQUIRE(result);
+
+    mpc.getPerformanceManager().lock()->drainQueue();
+    REQUIRE(program->getMidiProgramChange() == 128);
+}
+
+TEST_CASE("ProgramFileReader reads upper-bound MIDI program change byte", "[kaitai-pgm]")
+{
+    auto fs = cmrc::mpctest::get_filesystem();
+    auto file = fs.open("test/ProgramLoading/program1/PROGRAM1.PGM");
+    const std::string originalBytes(
+        std::string_view(file.begin(), file.end() - file.begin())
+    );
+
+    std::stringstream parseStream(
+        originalBytes,
+        std::ios::in | std::ios::out | std::ios::binary
+    );
+    kaitai::kstream parseIo(&parseStream);
+    mpc2000xl_pgm_t parsed(&parseIo);
+    parsed._read();
+    parsed.set_program_change(127);
+
+    std::stringstream writeStream(std::ios::in | std::ios::out | std::ios::binary);
+    kaitai::kstream writeIo(&writeStream);
+    parsed._set_io(&writeIo);
+    parsed._check();
+    parsed._write();
+
+    const auto rewrittenBytes = writeStream.str();
+    std::vector<char> programBytes(rewrittenBytes.begin(), rewrittenBytes.end());
+
+    mpc::Mpc mpc;
+    mpc::TestMpc::initializeTestMpcWithoutIoServices(mpc);
+    auto pgmFile = loadProgram1FileFromBytes(mpc, programBytes);
+
+    mpc::file::pgmreader::ProgramFileReader reader(pgmFile);
+    REQUIRE(reader.getSlider()->getProgramChange() == 127);
+}
+
+TEST_CASE("PgmFileToProgramConverter loads mutated note tune", "[kaitai-pgm]")
+{
+    auto fs = cmrc::mpctest::get_filesystem();
+    auto file = fs.open("test/ProgramLoading/program1/PROGRAM1.PGM");
+    const std::string originalBytes(
+        std::string_view(file.begin(), file.end() - file.begin())
+    );
+
+    std::stringstream parseStream(
+        originalBytes,
+        std::ios::in | std::ios::out | std::ios::binary
+    );
+    kaitai::kstream parseIo(&parseStream);
+    mpc2000xl_pgm_t parsed(&parseIo);
+    parsed._read();
+    parsed.note_parameters()->at(0)->set_tune(-120);
+
+    std::stringstream writeStream(std::ios::in | std::ios::out | std::ios::binary);
+    kaitai::kstream writeIo(&writeStream);
+    parsed._set_io(&writeIo);
+    parsed._check();
+    parsed._write();
+
+    const auto rewrittenBytes = writeStream.str();
+    std::vector<char> programBytes(rewrittenBytes.begin(), rewrittenBytes.end());
+
+    mpc::Mpc mpc;
+    mpc::TestMpc::initializeTestMpcWithoutIoServices(mpc);
+    auto pgmFile = loadProgram1FileFromBytes(mpc, programBytes);
+    auto program = mpc.getSampler()->createNewProgramAddFirstAvailableSlotAndThen({}).lock();
+    std::vector<std::string> soundNames;
+
+    auto result = mpc::disk::PgmFileToProgramConverter::loadFromFileAndConvert(
+        mpc, pgmFile, program, soundNames
+    );
+    REQUIRE(result);
+
+    mpc.getPerformanceManager().lock()->drainQueue();
+    REQUIRE(program->getNoteParameters(35)->getTune() == -120);
 }
