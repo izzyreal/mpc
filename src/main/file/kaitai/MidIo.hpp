4a71cc23537f52a1e3eb6023a165f93f179a0980
Izmar <3707432+izzyreal@users.noreply.github.com>
2026-06-22 21:50:40 +0200

    Add direct Kaitai MIDI save path

--- src/main/file/kaitai/MidIo.hpp
+++ src/main/file/kaitai/MidIo.hpp
@@ -26,7 +26,12 @@ namespace mpc::file::kaitai
     class MidIo
     {
     public:
+        static sequence_or_error loadBytes(
+            mpc::Mpc &,
+            const std::vector<char> &,
+            const std::string &fileNameWithoutExtension);
         static sequence_or_error load(mpc::Mpc &, const std::shared_ptr<disk::MpcFile> &);
+        static std::vector<char> saveBytes(const std::shared_ptr<sequencer::Sequence> &);
         static void save(const std::shared_ptr<sequencer::Sequence> &, const std::shared_ptr<std::ostream> &);
     };
 }
