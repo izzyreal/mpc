3f6ad86b795ea5f6a759b8c9cf964d17feeba8d2
Izmar <3707432+izzyreal@users.noreply.github.com>
2026-07-18 18:56:10 +0200

    Add tests

--- src/test/SequenceTest.cpp
+++ src/test/SequenceTest.cpp
@@ -9,10 +9,54 @@
 #include "lcdgui/screens/BarsScreen.hpp"
 #include "sequencer/EventRef.hpp"
 #include "sequencer/SequenceMessage.hpp"
+#include "sequencer/SequencerMessage.hpp"
 #include "sequencer/SequencerStateManager.hpp"
+
+#include <atomic>
+#include <thread>
 
 using namespace mpc::sequencer;
 using namespace mpc::lcdgui::screens;
+
+namespace
+{
+void insertTestNote(
+    const std::shared_ptr<Track> &track,
+    const mpc::Tick tick,
+    const int noteNumber)
+{
+    EventData eventData;
+    eventData.type = EventType::NoteOn;
+    eventData.tick = tick;
+    eventData.noteNumber = mpc::NoteNumber(noteNumber);
+    eventData.velocity = mpc::MaxVelocity;
+    eventData.duration = mpc::Duration(24);
+    track->acquireAndInsertEvent(eventData);
+}
+
+void enqueueBulkNote(
+    const std::shared_ptr<SequencerStateManager> &stateManager,
+    const mpc::SequenceIndex sequenceIndex,
+    const mpc::TrackIndex trackIndex,
+    const mpc::Tick tick,
+    const int noteNumber)
+{
+    auto &trackEvents = stateManager->trackEventsSnapshots[sequenceIndex];
+    trackEvents.clear();
+
+    EventData eventData;
+    eventData.type = EventType::NoteOn;
+    eventData.tick = tick;
+    eventData.noteNumber = mpc::NoteNumber(noteNumber);
+    eventData.velocity = mpc::MaxVelocity;
+    eventData.duration = mpc::Duration(24);
+    trackEvents[trackIndex].push_back(eventData);
+
+    UpdateSequenceEvents update{sequenceIndex};
+    update.trackSnapshots = &trackEvents;
+    stateManager->enqueue(update);
+}
+}
 
 TEST_CASE("Insert bar and move event forward", "[sequence]")
 {
@@ -132,4 +176,169 @@ TEST_CASE("UpdateSequenceTracks preserves existing events", "[sequence]")
     REQUIRE(track->getName() == "BulkMeta");
     REQUIRE(track->getBusType() == BusType::MIDI);
     REQUIRE(track->getDeviceIndex() == 3);
+}
+
+TEST_CASE("UpdateSequenceEvents appends imported events", "[sequence]")
+{
+    mpc::Mpc mpc;
+    mpc::TestMpc::initializeTestMpc(mpc);
+
+    const auto sequencer = mpc.getSequencer();
+    const auto stateManager = sequencer->getStateManager();
+    const auto seq = sequencer->getSelectedSequence();
+    seq->init(0);
+    stateManager->drainQueue();
+
+    const auto track = seq->getTrack(0);
+    insertTestNote(track, 12, 35);
+    stateManager->drainQueue();
+
+    enqueueBulkNote(
+        stateManager, seq->getSequenceIndex(), track->getIndex(), 48, 36);
+    stateManager->drainQueue();
+
+    REQUIRE(track->getEvents().size() == 2);
+    REQUIRE(track->getEvent(0)->getTick() == 12);
+    REQUIRE(track->getEvent(1)->getTick() == 48);
+}
+
+TEST_CASE("CopySequence copies event lists without sharing handles", "[sequence]")
+{
+    mpc::Mpc mpc;
+    mpc::TestMpc::initializeTestMpc(mpc);
+
+    const auto sequencer = mpc.getSequencer();
+    const auto stateManager = sequencer->getStateManager();
+    const auto source = sequencer->getSequence(0);
+    const auto dest = sequencer->getSequence(1);
+    source->init(0);
+    dest->init(0);
+    stateManager->drainQueue();
+
+    const auto sourceTrack = source->getTrack(0);
+    const auto destTrack = dest->getTrack(0);
+    insertTestNote(sourceTrack, 24, 35);
+    stateManager->drainQueue();
+
+    sequencer->copySequence(mpc::SequenceIndex(0), mpc::SequenceIndex(1));
+    stateManager->drainQueue();
+
+    REQUIRE(destTrack->getEvents().size() == 1);
+    REQUIRE(destTrack->getEvent(0)->getTick() == 24);
+    REQUIRE(destTrack->getEvent(0)->handle != sourceTrack->getEvent(0)->handle);
+}
+
+TEST_CASE("DeleteSequence clears event lists", "[sequence]")
+{
+    mpc::Mpc mpc;
+    mpc::TestMpc::initializeTestMpc(mpc);
+
+    const auto sequencer = mpc.getSequencer();
+    const auto stateManager = sequencer->getStateManager();
+    const auto seq = sequencer->getSequence(0);
+    seq->init(0);
+    stateManager->drainQueue();
+
+    const auto track = seq->getTrack(0);
+    insertTestNote(track, 24, 35);
+    stateManager->drainQueue();
+    REQUIRE(track->getEvents().size() == 1);
+
+    sequencer->deleteSequence(0);
+    stateManager->drainQueue();
+
+    REQUIRE(track->getEvents().empty());
+}
+
+TEST_CASE("EraseEvents removes matching events", "[sequence]")
+{
+    mpc::Mpc mpc;
+    mpc::TestMpc::initializeTestMpc(mpc);
+
+    const auto sequencer = mpc.getSequencer();
+    const auto stateManager = sequencer->getStateManager();
+    const auto seq = sequencer->getSelectedSequence();
+    seq->init(0);
+    stateManager->drainQueue();
+
+    const auto track = seq->getTrack(0);
+    insertTestNote(track, 12, 35);
+    insertTestNote(track, 48, 36);
+    stateManager->drainQueue();
+
+    stateManager->enqueue(EraseEvents{
+        seq->getSequenceIndex(), track->getIndex(), mpc::Tick(0),
+        mpc::Tick(24), 0, 0, NoteRange::All()});
+    stateManager->drainQueue();
+    stateManager->drainQueue();
+
+    REQUIRE(track->getEvents().size() == 1);
+    REQUIRE(track->getEvent(0)->getTick() == 48);
+}
+
+TEST_CASE("Track event readers tolerate concurrent sequence mutations",
+          "[sequence][sequencer-multithread]")
+{
+    mpc::Mpc mpc;
+    mpc::TestMpc::initializeTestMpc(mpc);
+
+    const auto sequencer = mpc.getSequencer();
+    const auto stateManager = sequencer->getStateManager();
+    const auto seq = sequencer->getSelectedSequence();
+    seq->init(0);
+    stateManager->drainQueue();
+
+    const auto track = seq->getTrack(0);
+    insertTestNote(track, 12, 35);
+    insertTestNote(track, 48, 36);
+    stateManager->drainQueue();
+
+    std::atomic<bool> stop{false};
+    std::atomic<int> snapshotsRead{0};
+
+    std::thread reader(
+        [&]
+        {
+            while (!stop.load())
+            {
+                for (const auto &event : track->getEvents())
+                {
+                    REQUIRE(event != nullptr);
+                    (void)event->getTick();
+                }
+                ++snapshotsRead;
+            }
+        });
+
+    for (int i = 0; i < 50; ++i)
+    {
+        enqueueBulkNote(
+            stateManager, seq->getSequenceIndex(), track->getIndex(),
+            mpc::Tick((i % 8) * 12), 37 + (i % 8));
+        stateManager->drainQueue();
+
+        auto &trackStates =
+            stateManager->trackStatesSnapshots[seq->getSequenceIndex()];
+        trackStates = SequenceTrackStatesSnapshot();
+        trackStates[0].name = "Stress";
+        trackStates[0].used = true;
+        UpdateSequenceTracks updateTracks{seq->getSequenceIndex()};
+        updateTracks.trackStates = &trackStates;
+        stateManager->enqueue(updateTracks);
+        stateManager->drainQueue();
+
+        stateManager->enqueue(EraseEvents{
+            seq->getSequenceIndex(), track->getIndex(), mpc::Tick(0),
+            mpc::Tick(24), 0, 0, NoteRange::All()});
+        stateManager->drainQueue();
+        stateManager->drainQueue();
+
+        sequencer->copySequence(seq->getSequenceIndex(), mpc::UndoSequenceIndex);
+        stateManager->drainQueue();
+    }
+
+    stop.store(true);
+    reader.join();
+
+    REQUIRE(snapshotsRead.load() > 0);
 }
